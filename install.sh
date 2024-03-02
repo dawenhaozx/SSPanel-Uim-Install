1a2583b11487cff0f0a435c406651ea62f7da2a1
dawenhaozx <58341754+dawenhaozx@users.noreply.github.com>
2024-03-02 20:15:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 # SSPanel-Uim版本： Jan 17, 2024
 git clone -b dev https://github.com/dawenhaozx/SSPanel-Uim
 cd SSPanel-Uim
-# git reset --hard 6913425a23e3302122eb8dcfe86afada611bec9c
-# cd -
+git reset --hard f6658c06d8c307479327dda22c7a43b723cfcbc2
+cd -
 # Dev
 # git clone https://github.com/Meowpay/SSPanel-Uim-Dev.git SSPanel-Uim
 # 设置权限
